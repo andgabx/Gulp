d8dea88b481d4226a26e353bf0431d5d8c016991
Anderson Gabriel <gabrielvalencasa98@gmail.com>
2024-10-27 14:26:58 -0300

    Testing compressed autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
 
+const sass = gulpSass(dartSass);
 
 function SassCompiler() {
     return gulp
-    .src('css/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('css/'))
+        .src('css/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'], cascade: false }))
+        .pipe(gulp.dest('css/'));
 }
 
 gulp.task('default', SassCompiler);
